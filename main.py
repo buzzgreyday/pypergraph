af28741a432f9a7bb7a1da9d7e7fffc4c28d11c3
buzzgreyday <micbr@protonmail.com>
2024-12-18 17:24:19 +0100

    test okay

--- main.py
+++ main.py
@@ -45,9 +45,9 @@ class Bip39:
         entropy = mnemo.to_entropy(words)
         return {"mnemo": mnemo, "words": words, "seed": seed, "entropy": entropy}
 
-    def get_seed_from_mnemonic(self, mnemonic_phrase: str):
+    def get_seed_from_mnemonic(self, words: str):
         mnemo = Mnemonic(self.language)
-        return mnemo.to_seed(mnemonic_phrase)
+        return mnemo.to_seed(words)
 
 class Bip32:
     @staticmethod
@@ -133,7 +133,7 @@ def main():
     public_key = bip32.get_public_key_from_private_hex(private_key_hex=private_key)
     dag_addr = wallet.get_dag_address_from_public_key_hex(public_key_hex=public_key)
     print("Values:", mnemonic_values, "\nPrivate Key: " + private_key, "\nPublic Key: " + public_key, "\nDAG Address: " + dag_addr)
-    derived_seed = bip39.get_seed_from_mnemonic(mnemonic_phrase=mnemonic_values["words"])
+    derived_seed = bip39.get_seed_from_mnemonic(words=mnemonic_values["words"])
     derived_private_key = bip32.get_private_key_from_seed(seed_bytes=derived_seed)
     derived_public_key = bip32.get_public_key_from_private_hex(private_key_hex=derived_private_key)
     derived_dag_addr = wallet.get_dag_address_from_public_key_hex(public_key_hex=derived_public_key)
