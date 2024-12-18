0fefe71ae775f97a8984d5ed342eeb0cb1caa416
buzzgreyday <micbr@protonmail.com>
2024-12-18 17:18:34 +0100

    add mnemonic to seed

--- main.py
+++ main.py
@@ -45,6 +45,10 @@ class Bip39:
         entropy = mnemo.to_entropy(words)
         return {"mnemo": mnemo, "words": words, "seed": seed, "entropy": entropy}
 
+    def get_seed_from_mnemonic(self, mnemonic_phrase: str):
+        mnemo = Mnemonic(self.language)
+        return mnemo.to_seed(mnemonic_phrase)
+
 class Bip32:
     @staticmethod
     def get_root_key_from_seed(seed_bytes):
@@ -129,6 +133,7 @@ def main():
     public_key = bip32.get_public_key_from_private_hex(private_key_hex=private_key)
     dag_addr = wallet.get_dag_address_from_public_key_hex(public_key_hex=public_key)
     print("Values:", mnemonic_values, "\nPrivate Key: " + private_key, "\nPublic Key: " + public_key, "\nDAG Address: " + dag_addr)
+    print(bip39.get_seed_from_mnemonic(mnemonic_phrase=mnemonic_values["words"]))
 
 if __name__ == "__main__":
     main()
