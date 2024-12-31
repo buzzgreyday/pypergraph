834aa208c91ca44d03f1047a069b79a3e5db1f18
buzzgreyday <micbr@protonmail.com>
2024-12-31 08:58:25 +0100

    Exception

--- dag_network/api.py
+++ dag_network/api.py
@@ -2,6 +2,14 @@ from .constants import DEFAULT_L1_BASE_URL
 
 import requests
 
+class TransactionApiError(Exception):
+    def __init__(self, error_code):
+        self.error = error_code
+
+    def __str__(self):
+        return f"Error processing request (status code: {self.error})"
+
+
 class API:
     @staticmethod
     def get_last_reference(dag_address: str):
@@ -28,5 +36,8 @@ class API:
         response = requests.post(url, headers=headers, json=tx)
         if response.status_code == 200:
             return response.json()
+        elif response.status_code in (400, 500):
+            print(response.json())
+            raise TransactionApiError(response.status_code)
         else:
-            response.raise_for_status()
+            return response.json()
