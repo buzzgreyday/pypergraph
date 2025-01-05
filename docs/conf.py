d5749709099680213bdcf538843cb5e0e72d74d5
buzzgreyday <micbr@protonmail.com>
2025-01-05 05:53:50 +0100

    Setup docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,12 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
-import sys
-
 project = 'pypergraph'
 copyright = '2025, Michael Brummer Ringdal'
 author = 'Michael Brummer Ringdal'
+release = '2025.0.0a3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -26,10 +24,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_theme = 'alabaster'
 html_static_path = ['_static']
-
-sys.path.insert(0, os.path.abspath(".."))  # Ensure your package is on the path
-
-# Configure theme
-html_theme = "sphinx_rtd_theme"
-
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. pypergraph documentation master file, created by
-   sphinx-quickstart on Sun Jan  5 05:21:43 2025.
+   sphinx-quickstart on Sun Jan  5 05:52:40 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
@@ -15,4 +15,3 @@ documentation for details.
    :maxdepth: 2
    :caption: Contents:
 
-modules
