28cd95295d63c4aa2bcd902b211cf892b9bea81b
buzzgreyday <micbr@protonmail.com>
2025-01-05 05:49:21 +0100

    Setup docs

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+project = 'pypergraph'
+copyright = '2025, Michael Brummer Ringdal'
+author = 'Michael Brummer Ringdal'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
+
+sys.path.insert(0, os.path.abspath(".."))  # Ensure your package is on the path
+
+# Configure theme
+html_theme = "sphinx_rtd_theme"
+
