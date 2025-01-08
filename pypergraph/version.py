d530923f2c2343419f1bdb464507fc84433a6599
buzzgreyday <micbr@protonmail.com>
2025-01-08 12:40:50 +0100

    Fix version and docs

--- pypergraph/version.py
+++
@@ -1,9 +0,0 @@
-import tomllib  # Use `toml` if you're targeting Python <3.11
-from pathlib import Path
-
-def get_version():
-    pyproject_path = Path(__file__).parent.parent / "pyproject.toml"
-    with pyproject_path.open("rb") as f:
-        data = tomllib.load(f)
-    return data["project"]["version"]
-
