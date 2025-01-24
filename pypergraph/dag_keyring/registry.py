fc09f777c2a614908f9641482d9d3a4b2077e162
buzzgreyday <micbr@protonmail.com>
2025-01-24 02:37:21 +0100

    chor: commenting registry

--- pypergraph/dag_keyring/registry.py
+++ pypergraph/dag_keyring/registry.py
@@ -9,7 +9,19 @@ class KeyringRegistry:
             KeyringNetwork.Ethereum.value: EcdsaAccount,
         }
 
+    def register_account_classes(self, data: dict):
+        """
+        :param data: { KeyringNetwork.Network.value: AccountClass, ... }
+        :return:
+        """
+        self.registry = data
+
     def create_account(self, network):
+        """
+        Determine the account class dependent on network.
+        :param network: E.g. KeyringNetwork.Constellation.value
+        :return: Account class.
+        """
 
         if not network:
             raise ValueError(f"Unsupported network: {network}")
