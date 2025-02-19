a6d00d8521893a387d1c2fb60c4c9cbdd5bbb6b7
buzzgreyday <micbr@protonmail.com>
2025-02-19 09:51:24 +0100

    chor: Clean

--- pypergraph/dag_core/models/transaction.py
+++ pypergraph/dag_core/models/transaction.py
@@ -9,15 +9,37 @@ from pypergraph.dag_core.constants import DAG_MAX
 from pypergraph.dag_core.models.account import LastReference
 
 
+class BaseTransaction(BaseModel):
+    source: str  # Validated in validate_dag_address
+    destination: str  # Validated in validate_dag_address
+
+    @model_validator(mode='before')
+    def validate_dag_address(cls, values):
+        for address in (values.get('source'), values.get('destination')):
+            if address:
+                valid_len = len(address) == 40
+                valid_prefix = address.startswith("DAG")
+                valid_parity = address[3].isdigit() and 0 <= int(address[3]) < 10
+                base58_part = address[4:]
+                valid_base58 = (
+                    len(base58_part) == 36 and base58_part == base58.b58encode(base58.b58decode(base58_part)).decode()
+                )
+
+                # If any validation fails, raise an error
+                if not (valid_len and valid_prefix and valid_parity and valid_base58):
+                    raise ValueError(f"Invalid address: {address}")
+
+        return values
+
+
+
 class PostTransactionResponse(BaseModel):
     hash: constr(pattern=r"^[a-fA-F0-9]{64}$")
 
     def __repr__(self) -> str:
         return f"PostTransactionResponse(hash={self.hash!r})"
 
-class PendingTransaction(BaseModel):
-    source: str # Validated below
-    destination: str # Validated below
+class PendingTransaction(BaseTransaction):
     amount: int = Field(ge=0, le=DAG_MAX)
     fee: int = Field(ge=0, le=DAG_MAX)
     parent_hash: constr(pattern=r"^[a-fA-F0-9]{64}$") = Field(..., alias="parentHash")
@@ -48,29 +70,8 @@ class PendingTransaction(BaseModel):
                 f"parent_ordinal={self.parent_ordinal}, salt={self.salt}, "
                 f"transaction_hash={self.transaction_hash}, status={self.status})")
 
-    @model_validator(mode='before')
-    def validate_dag_address(cls, values):
-        for address in (values.get('source'), values.get('destination')):
-            if address:
 
-                valid_len = len(address) == 40
-                valid_prefix = address.startswith("DAG")
-                valid_parity = address[3].isdigit() and 0 <= int(address[3]) < 10
-                base58_part = address[4:]
-                valid_base58 = (
-                    len(base58_part) == 36 and base58_part == base58.b58encode(base58.b58decode(base58_part)).decode()
-                )
-
-                # If any validation fails, raise an error
-                if not (valid_len and valid_prefix and valid_parity and valid_base58):
-                    raise ValueError(f"CurrencySnapshot :: Invalid address: {address}")
-
-        return values
-
-
-class Transaction(BaseModel):
-    source: str # Validated below
-    destination: str # Validated below
+class Transaction(BaseTransaction):
     amount: int = Field(ge=0, le=DAG_MAX)
     fee: int = Field(ge=0, le=DAG_MAX)
     parent: LastReference
@@ -80,24 +81,6 @@ class Transaction(BaseModel):
         return (f"TransactionValue(source={self.source}, destination={self.destination}, "
                 f"amount={self.amount}, fee={self.fee}, parent={self.parent}, salt={self.salt})")
 
-    @model_validator(mode='before')
-    def validate_dag_address(cls, values):
-        for address in (values.get('source'), values.get('destination')):
-            if address:
-
-                valid_len = len(address) == 40
-                valid_prefix = address.startswith("DAG")
-                valid_parity = address[3].isdigit() and 0 <= int(address[3]) < 10
-                base58_part = address[4:]
-                valid_base58 = (
-                    len(base58_part) == 36 and base58_part == base58.b58encode(base58.b58decode(base58_part)).decode()
-                )
-
-                # If any validation fails, raise an error
-                if not (valid_len and valid_prefix and valid_parity and valid_base58):
-                    raise ValueError(f"CurrencySnapshot :: Invalid address: {address}")
-
-        return values
 
     @computed_field
     @property
@@ -158,11 +141,9 @@ class SignedTransaction(BaseModel):
 
 
 
-class BlockExplorerTransaction(BaseModel):
+class BlockExplorerTransaction(BaseTransaction):
     hash: constr(pattern=r"^[a-fA-F0-9]{64}$")
     amount: int = Field(ge=0, le=DAG_MAX)
-    source: str # Validated below
-    destination: str # Validate below
     fee: int = Field(ge=0, lt=DAG_MAX)
     parent: LastReference
     salt: Optional[int] = Field(default=None, ge=0)
@@ -186,24 +167,6 @@ class BlockExplorerTransaction(BaseModel):
     def process_transactions(cls, data: List[dict], meta: Optional[dict] = None) -> List["BlockExplorerTransaction"]:
         return [cls.model_validate({**tx, "meta": meta}) for tx in data]
 
-    @model_validator(mode='before')
-    def validate_dag_address(cls, values):
-        for address in (values.get('source'), values.get('destination')):
-            if address:
-
-                valid_len = len(address) == 40
-                valid_prefix = address.startswith("DAG")
-                valid_parity = address[3].isdigit() and 0 <= int(address[3]) < 10
-                base58_part = address[4:]
-                valid_base58 = (
-                    len(base58_part) == 36 and base58_part == base58.b58encode(base58.b58decode(base58_part)).decode()
-                )
-
-                # If any validation fails, raise an error
-                if not (valid_len and valid_prefix and valid_parity and valid_base58):
-                    raise ValueError(f"CurrencySnapshot :: Invalid address: {address}")
-
-        return values
 
     class ConfigDict:
         population_by_name = True
