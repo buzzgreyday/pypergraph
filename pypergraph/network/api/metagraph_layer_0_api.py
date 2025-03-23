3248a1b5bc9e8f369ee8bab1261b295b18d4abeb
buzzgreyday <micbr@protonmail.com>
2025-03-23 16:12:58 +0100

    chor: Refactor network code (metagraph_layer_0_api) to use with blocks. We also need to handle if certain layers are not provided.

--- pypergraph/network/api/metagraph_layer_0_api.py
+++ pypergraph/network/api/metagraph_layer_0_api.py
@@ -1,4 +1,3 @@
-from pypergraph.core.rest_api_client import RestAPIClient
 from pypergraph.network.api.layer_0_api import L0Api
 from pypergraph.network.models import TotalSupply, Balance
 
@@ -7,28 +6,18 @@ class ML0Api(L0Api):
     def __init__(self, host: str):
         super().__init__(host)
 
-    @property
-    def service(self) -> RestAPIClient:
-        if not self._service:
-            raise ValueError("ML0Api :: Metagraph layer 0 host is not configured.")
-        return self._service
-
-    def config(self, host: str):
-        """Reconfigure the RestAPIClient's base URL."""
-        self._service = RestAPIClient(host)
-
     async def get_total_supply(self) -> TotalSupply:
-        result = await self.service.get("/currency/total-supply")
+        result = await self._make_request("GET", "/currency/total-supply")
         return TotalSupply(**result)
 
     async def get_total_supply_at_ordinal(self, ordinal: int) -> TotalSupply:
-        result = await self.service.get(f"/currency/{ordinal}/total-supply")
+        result = await self._make_request("GET", f"/currency/{ordinal}/total-supply")
         return TotalSupply(**result)
 
     async def get_address_balance(self, address: str) -> Balance:
-        result = await self.service.get(f"/currency/{address}/balance")
+        result = await self._make_request("GET", f"/currency/{address}/balance")
         return Balance(**result, meta=result.get("meta"))
 
     async def get_address_balance_at_ordinal(self, ordinal: int, address: str) -> Balance:
-        result = await self.service.get(f"/currency/{ordinal}/{address}/balance")
+        result = await self._make_request("GET", f"/currency/{ordinal}/{address}/balance")
         return Balance(**result, meta=result.get("meta"))
