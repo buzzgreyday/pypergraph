f455727cb841f95cbc064082fda63c9fe91fdeb5
buzzgreyday <micbr@protonmail.com>
2025-03-23 15:50:08 +0100

    chor: Refactor network code (layer_0_api) to use with blocks

--- pypergraph/network/api/layer_0_api.py
+++ pypergraph/network/api/layer_0_api.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Union
 
 from prometheus_client.parser import text_string_to_metric_families
 
@@ -24,22 +24,28 @@ def _handle_metrics(response: str) -> List[Dict[str, Any]]:
             })
     return metrics
 
+
 class L0Api:
     def __init__(self, host: str):
-        self._service = RestAPIClient(host) if host else None
-
-    @property
-    def service(self) -> RestAPIClient:
-        if not self._service:
+        if not host:
             raise ValueError("L0Api :: Layer 0 host is not configured.")
-        return self._service
+        self._host = host
 
     def config(self, host: str):
         """Reconfigure the RestAPIClient's base URL."""
-        self._service = RestAPIClient(host)
+        if not host:
+            raise ValueError("L0Api :: Layer 0 host is not configured.")
+        self._host = host
+
+    async def _make_request(self, method: str, endpoint: str, params: Dict[str, Any] = None, payload: Dict[str, Any] = None) -> Union[Dict, List, str]:
+        """
+        Helper function to create a new RestAPIClient instance and make a request.
+        """
+        async with RestAPIClient(base_url=self._host) as client:
+            return await client.request(method=method, endpoint=endpoint, params=params, payload=payload)
 
     async def get_cluster_info(self) -> List[PeerInfo]:
-        result = await self.service.get("/cluster/info")
+        result = await self._make_request("GET", "/cluster/info")
         return PeerInfo.process_cluster_peers(data=result)
 
     async def get_metrics(self) -> List[Dict[str, Any]]:
@@ -48,25 +54,25 @@ class L0Api:
 
         :return: Prometheus output as a list of dictionaries.
         """
-        response = await self.service.get("/metrics")
+        response = await self._make_request("GET", "/metrics")
         return _handle_metrics(response)
 
     async def get_total_supply(self) -> TotalSupply:
-        result = await self.service.get("/dag/total-supply")
+        result = await self._make_request("GET", "/dag/total-supply")
         return TotalSupply(**result)
 
     async def get_address_balance(self, address: str) -> Balance:
-        result = await self.service.get(f"/dag/{address}/balance")
+        result = await self._make_request("GET", f"/dag/{address}/balance")
         return Balance(**result, meta=result.get("meta"))
 
     async def get_latest_snapshot(self) -> GlobalSnapshot:
-        result = await self.service.get("/global-snapshots/latest")
+        result = await self._make_request("GET", "/global-snapshots/latest")
         return GlobalSnapshot(**result)
 
     async def get_latest_snapshot_ordinal(self) -> Ordinal:
-        result = await self.service.get("/global-snapshots/latest/ordinal")
+        result = await self._make_request("GET", "/global-snapshots/latest/ordinal")
         return Ordinal(**result)
 
     async def post_state_channel_snapshot(self, address: str, snapshot: dict):
         # TODO: Add validation
-        return await self.service.post(f"/state-channel/{address}/snapshot", payload=snapshot)
+        return await self._make_request("POST", f"/state-channel/{address}/snapshot", payload=snapshot)
