9babd30f8acaa0aea085d7b06c4d7bf11cb62ecd
buzzgreyday <micbr@protonmail.com>
2025-03-23 15:43:36 +0100

    chor: Refactor network code (load_balancer_api) to use with blocks

--- pypergraph/network/api/load_balancer_api.py
+++ pypergraph/network/api/load_balancer_api.py
@@ -1,5 +1,4 @@
-from typing import List, Dict, Any
-
+from typing import List, Dict, Any, Union
 from prometheus_client.parser import text_string_to_metric_families
 
 from pypergraph.core.rest_api_client import RestAPIClient
@@ -24,19 +23,27 @@ def _handle_metrics(response: str) -> List[Dict[str, Any]]:
             })
     return metrics
 
+
 class LoadBalancerApi:
     def __init__(self, host: str):
-        self._service = RestAPIClient(host) if host else None
-
-    @property
-    def service(self) -> RestAPIClient:
-        if not self._service:
+        if not host:
             raise ValueError("LoadBalancerApi :: Load balancer host is not configured.")
-        return self._service
+        self._host = host
 
     def config(self, host: str):
         """Reconfigure the RestAPIClient's base URL."""
-        self._service = RestAPIClient(host)
+        if not host:
+            raise ValueError("LoadBalancerApi :: Load balancer host is not configured.")
+        self._host = host
+
+    async def _make_request(
+        self, method: str, endpoint: str, params: Dict[str, Any] = None, payload: Dict[str, Any] = None
+    ) -> Union[Dict, List, str]:
+        """
+        Helper function to create a new RestAPIClient instance and make a request.
+        """
+        async with RestAPIClient(base_url=self._host) as client:
+            return await client.request(method=method, endpoint=endpoint, params=params, payload=payload)
 
     async def get_metrics(self) -> List[Dict[str, Any]]:
         """
@@ -44,29 +51,29 @@ class LoadBalancerApi:
 
         :return: Prometheus output as a list of dictionaries.
         """
-        response = await self.service.get("/metrics")
+        response = await self._make_request("GET", "/metrics")
         return _handle_metrics(response)
 
     async def get_address_balance(self, address: str) -> Balance:
-        result = await self.service.get(f"/dag/{address}/balance")
+        result = await self._make_request("GET", f"/dag/{address}/balance")
         return Balance(**result, meta=result.get("meta"))
 
     async def get_last_reference(self, address: str) -> LastReference:
-        result = await self.service.get(f"/transactions/last-reference/{address}")
+        result = await self._make_request("GET", f"/transactions/last-reference/{address}")
         return LastReference(**result)
 
     async def get_total_supply(self) -> TotalSupply:
-        result = await self.service.get("/total-supply")
+        result = await self._make_request("GET", "/total-supply")
         return TotalSupply(**result)
 
     async def post_transaction(self, tx: Dict[str, Any]):
-        result = await self.service.post("/transactions", payload=tx)
+        result = await self._make_request("POST", "/transactions", payload=tx)
         return result
 
     async def get_pending_transaction(self, tx_hash: str) -> PendingTransaction:
-        result = await self.service.get(f"/transactions/{tx_hash}")
+        result = await self._make_request("GET", f"/transactions/{tx_hash}")
         return PendingTransaction(**result)
 
     async def get_cluster_info(self) -> List[PeerInfo]:
-        result = await self.service.get("/cluster/info")
+        result = await self._make_request("GET", "/cluster/info")
         return PeerInfo.process_cluster_peers(data=result)
