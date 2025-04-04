ddc01f0ff22191d7f00ff7894741182b88558cba
buzzgreyday <micbr@protonmail.com>
2025-04-04 19:18:29 +0200

    fix: signing security, ecdsa -> pyca/cryptography

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('../../pypergraph/'))
 project = 'Pypergraph'
 copyright = '2025, Michael Brummer Ringdal'
 author = 'Michael Brummer Ringdal'
-release = '0.0.2'
+release = '0.0.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
