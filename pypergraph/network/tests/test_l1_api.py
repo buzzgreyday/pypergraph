65908f9dc1e31610dfe1c1f54e48e52499284bc8
buzzgreyday <micbr@protonmail.com>
2025-06-08 10:49:06 +0200

    test: metagraph tx

--- pypergraph/network/tests/test_l1_api.py
+++ pypergraph/network/tests/test_l1_api.py
@@ -62,6 +62,36 @@ class TestMockedL1API:
         )
         await account.network.post_transaction(tx)
 
+    @pytest.mark.asyncio
+    async def test_post_metagraph_currency_transaction(self, network, httpx_mock: HTTPXMock, mock_l1_api_responses):
+        from .secret import mnemo, to_address, from_address
+
+        account = DagAccount()
+        account.login_with_seed_phrase(mnemo)
+        account_metagraph_client = MetagraphTokenClient(
+            account=account,
+            metagraph_id="DAG7ChnhUF7uKgn8tXy45aj4zn9AFuhaZr8VXY43",
+            l0_host="http://elpaca-l0-2006678808.us-west-1.elb.amazonaws.com:9100",
+            currency_l1_host="http://elpaca-cl1-1512652691.us-west-1.elb.amazonaws.com:9200",
+        )
+        httpx_mock.add_response(
+            url="http://elpaca-cl1-1512652691.us-west-1.elb.amazonaws.com:9200/transactions/last-reference/DAG0zJW14beJtZX2BY2KA9gLbpaZ8x6vgX4KVPVX",
+            json=mock_l1_api_responses["last_ref"])
+        # Generate signed tx
+        last_ref = await account_metagraph_client.network.get_address_last_accepted_transaction_ref(
+            address=from_address
+        )
+        tx, hash_ = await account_metagraph_client.account.generate_signed_transaction(
+            to_address=to_address, amount=10000000, fee=2000000, last_ref=last_ref
+        )
+        httpx_mock.add_response(
+            method="POST",
+            url="http://elpaca-cl1-1512652691.us-west-1.elb.amazonaws.com:9200/transactions",  # adjust if needed
+            json=mock_l1_api_responses["post_transaction"],
+            status_code=200
+        )
+        await account_metagraph_client.network.post_transaction(tx=tx)
+
 
 @pytest.mark.integration
 class TestIntegrationL1API:
@@ -135,3 +165,5 @@ class TestIntegrationL1API:
             raise
         except httpx.ReadTimeout:
             pytest.skip("Skipping due to timeout")
+        except httpx.ReadError:
+            pytest.skip("Did not receive any data from the network.")
