09b34708cb982683532697095b377cf3932fea3c
buzzgreyday <micbr@protonmail.com>
2025-06-20 14:58:22 +0200

    tests: add fixtures

--- pypergraph/account/tests/test_account.py
+++ pypergraph/account/tests/test_account.py
@@ -6,6 +6,8 @@ from pypergraph.core.exceptions import NetworkError
 from pypergraph.account import DagAccount, MetagraphTokenClient
 from pypergraph.network.models.transaction import PendingTransaction
 
+from pypergraph.network.tests.conftest import mock_l1_api_responses, mock_block_explorer_responses
+
 
 @pytest.mark.account
 class TestAccount:
